2f1a81addf7dfec39e3d8f151a902562ab69d631
ulfangor <inesanzil@gmail.com>
2026-02-18 16:01:38 +0100

    énigme 1 terminée

---
+++ Assets/Scenes/LobbyScene/Scripts/Registre.cs
@@ -0,0 +1,68 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Registre : MonoBehaviour
+{
+
+    public bool FirstPuzzleSolved = false;
+
+    public GameObject Registre_part1;
+    public GameObject Registre_part2;
+    public GameObject Registre_part3;
+
+    public TMP_InputField roomInput;
+    public AudioSource correctSound;
+    public AudioSource wrongSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void ClearField()
+    {
+        if (roomInput != null)
+            roomInput.text = string.Empty;
+    }
+
+    public void CheckAnswer()
+    {
+        string answer = roomInput.text.Trim();
+        
+
+        if (answer == "101")
+        {
+            correctSound?.Play();
+            FirstPuzzleSolved=true;
+            Registre_part2.SetActive(false);
+            Registre_part3.SetActive(true);
+        }
+        else
+        {
+            wrongSound?.Play();
+            ClearField();
+        }
+    }
+
+    public void touchIconClicked()
+    {
+        if (!FirstPuzzleSolved) 
+        { 
+            Registre_part1.SetActive(true);
+        }
+        else
+        {
+            Registre_part3.SetActive(true);
+        }
+    }
+
+}
